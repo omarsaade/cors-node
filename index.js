007f88671c5b723170bc4d961ac3d2495a8a26c6
omar saadeh <omarsaadeh11@gmail.com>
2023-01-30 10:48:35 -0800

    init commit

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const express = require("express");
-const app = express();
-module.exports = function () {
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-    next();
-  });
+module.exports = function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  next();
 };
